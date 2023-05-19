39c41cbd91d7cbea53b95ed506b96e47883a2c4a
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2023-05-19 20:58:42 +0700

    Create NewView.swift

---
+++ SaladMaker/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  SaladMaker
+//
+//  Created by Timotius Satrio on 19/05/23.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
