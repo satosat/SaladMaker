40f879025a6606efe972ed2cf0e4223dba912aa6
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2023-05-19 21:00:17 +0700

    Create ConflictingView.swift

---
+++ SaladMaker/ConflictingView.swift
@@ -0,0 +1,20 @@
+//
+//  ConflictingView.swift
+//  SaladMaker
+//
+//  Created by Timotius Satrio on 19/05/23.
+//
+
+import SwiftUI
+
+struct ConflictingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConflictingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConflictingView()
+    }
+}
